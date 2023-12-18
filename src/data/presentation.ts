5ffe1bf2008ea705e34b7cde4c4913b535602147
Jonathan Rodenas <jonathan.rodenas@bemyvega.com>
2023-12-18 02:10:14 +0100

    Update presentation description

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,7 +16,7 @@ const presentation: Presentation = {
   title: "WELCOME",
   //profile: "/picture.jpg",
   description:
-    "Hello, I'm a *spanish DevOps Cloud Engineer* with over *3 years* of experience. I am currently working with *AWS and Terraform*. Outside of work, I collect mechanical keyboards, play the guitar, learn about cloud technologies and exercise.",
+    "Hi! My name is *Jonathan Ródenas López*. I'm a *DevOps Cloud Engineer* with over *3 years* of experience. I am currently working with *AWS and Terraform*. Outside of work, I collect mechanical keyboards, play the guitar, learn about cloud technologies and exercise.",
   socials: [
     {
       label: "X",
